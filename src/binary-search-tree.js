ad458a86fa0d218ea92b87d945f887af1062b42a
Viktoria Kondrashova <kavidmi@gmail.com>
2024-12-23 16:59:44 +0300

    implement binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,39 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 
 class BinarySearchTree {
-
   constructor() {
     this.treeRoot = null;
   }
 
   root() {
-    if(!this.treeRoot) {
-      return null;
-    } else {
-      return this.treeRoot;
-    }
+    return !this.treeRoot ? null : this.treeRoot;
   }
 
   add(data) {
     this.treeRoot = addWithin(this.treeRoot, data);
 
     function addWithin(node, data) {
-      if(!node) {
-        return new Node(data);
-      }
+      if (!node) return new Node(data);
 
-      if(node.data === data) {
-        return node;
-      }
+      if (node.data === data) return node;
 
-      if(data < node.data) {
+      if (data < node.data) {
         node.left = addWithin(node.left, data);
       } else {
         node.right = addWithin(node.right, data);
@@ -47,15 +38,11 @@ class BinarySearchTree {
     return searchWithin(this.treeRoot, data);
 
     function searchWithin(node, data) {
-      if(!node) {
-        return false;
-      }
+      if (!node) return false;
 
-      if(node.data === data) {
-        return true;
-      }
+      if (node.data === data) return true;
 
-      if(data < node.data) {
+      if (data < node.data) {
         return searchWithin(node.left, data);
       } else {
         return searchWithin(node.right, data);
@@ -67,15 +54,11 @@ class BinarySearchTree {
     return findhWithin(this.treeRoot, data);
 
     function findhWithin(node, data) {
-      if(!node) {
-        return null;
-      }
+      if (!node) return null;
 
-      if(node.data === data) {
-        return node;
-      }
+      if (node.data === data) return node;
 
-      if(data < node.data) {
+      if (data < node.data) {
         return findhWithin(node.left, data);
       } else {
         return findhWithin(node.right, data);
@@ -87,33 +70,31 @@ class BinarySearchTree {
     this.treeRoot = removeNode(this.treeRoot, data);
 
     function removeNode(node, data) {
-      if(!node) {
-        return null;
-      }
+      if (!node) return null;
 
-      if(data < node.data) {
+      if (data < node.data) {
         node.left = removeNode(node.left, data);
         return node;
       } else if (node.data < data) {
         node.right = removeNode(node.right, data);
         return node;
       } else {
-        if(!node.left && !node.right) {
+        if (!node.left && !node.right) {
           return null;
         }
 
-        if(!node.left) {
+        if (!node.left) {
           node = node.right;
           return node;
         }
 
-        if(!node.right) {
+        if (!node.right) {
           node = node.left;
           return node;
         }
 
         let minFromRight = node.right;
-        while(minFromRight.left) {
+        while (minFromRight.left) {
           minFromRight = minFromRight.left;
         }
         node.data = minFromRight.data;
@@ -126,30 +107,26 @@ class BinarySearchTree {
   }
 
   min() {
-    if(!this.treeRoot) {
-      return null;
-    }
+    if (!this.treeRoot) return null;
+
     let node = this.treeRoot;
-    
-    while(node.left) {
-      node = node.left;
-    }
+
+    while (node.left) node = node.left;
+
     return node.data;
   }
 
   max() {
-    if(!this.treeRoot) {
-      return null;
-    }
+    if (!this.treeRoot) return null;
+
     let node = this.treeRoot;
-    
-    while(node.right) {
-      node = node.right;
-    }
+
+    while (node.right) node = node.right;
+
     return node.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
